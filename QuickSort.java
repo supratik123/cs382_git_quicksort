4eed53442cef73d36c18f3238c0fbe16ad933ef5
sacusa <sudhanshugupta96@gmail.com>
2018-09-29 18:47:56 +0530

    Add QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,9 @@
+class QuickSort {
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    static void doSort(int [] arr, int low, int high) {
+        return;
+    }
+}
